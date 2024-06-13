dfc6eaa02f9cb5410a87ee698329a1f644e1372d
MohammedLiyakath <mdliyakath48@gmail.com>
2024-06-14 00:42:40 +0530

    Added the qr reader

--- src/main/java/com/hackatic/ReadingQR/controller/QRReaderController.java
+++ src/main/java/com/hackatic/ReadingQR/controller/QRReaderController.java
@@ -1,11 +1,16 @@
 package com.hackatic.ReadingQR.controller;
 
+import com.google.zxing.NotFoundException;
 import com.hackatic.ReadingQR.model.ApiResponse;
 import com.hackatic.ReadingQR.service.QRReaderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 @RestController
 public class QRReaderController {
@@ -16,8 +21,25 @@ public class QRReaderController {
         this.qrReaderService = qrReaderService;
     }
     @GetMapping("/readQR")
-    public ResponseEntity<ApiResponse> readQR(){
-        ApiResponse response = qrReaderService.getImageURL();
-        return ResponseEntity.ok(response);
+    public ResponseEntity<String> readQR() throws NotFoundException, IOException {
+        try {
+            ApiResponse response = qrReaderService.getImageURL();
+            return ResponseEntity.ok(qrReaderService.getStringFromQR(response.getImage_url()));
+        }
+        catch (MalformedURLException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Invalid URL format: " + e.getMessage());
+        } catch (IOException e) {
+            if (e.getMessage().contains("404")) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("Image not found at the provided URL.");
+            } else if (e.getMessage().contains("503")) {
+                return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Service temporarily unavailable. Please try again later.");
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body("Error downloading image: " + e.getMessage());
+            }
+        }
     }
 }
--- src/main/java/com/hackatic/ReadingQR/service/QRReaderService.java
+++ src/main/java/com/hackatic/ReadingQR/service/QRReaderService.java
@@ -1,9 +1,21 @@
 package com.hackatic.ReadingQR.service;
 
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
 import com.hackatic.ReadingQR.model.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 @Service
 public class QRReaderService {
@@ -19,4 +31,32 @@ public class QRReaderService {
         String url = "https://hackattic.com/challenges/reading_qr/problem?access_token=b50d4a3dba3c9045";
         return restTemplate.getForObject(url, ApiResponse.class);
     }
+
+    public String getStringFromQR(String url) throws IOException, NotFoundException {
+        BufferedImage bufferedImage = downloadImage(url);
+        if(bufferedImage == null){
+            throw new IOException("Could not download image from URL");
+        }
+        return decodeQR(bufferedImage);
+    }
+
+    private String decodeQR(BufferedImage bufferedImage) throws NotFoundException {
+        try {
+            BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(bufferedImage)));
+            Result result = new MultiFormatReader().decode(binaryBitmap);
+            return result.getText();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public BufferedImage downloadImage(String url) throws IOException {
+        try(InputStream inputStream = new URL(url).openStream()){
+            return ImageIO.read(inputStream);
+        }
+    }
+
+
 }
